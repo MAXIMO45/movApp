26c1f13c97c1acc9e46e190620ad6bf0b08c36a1
DRLuis25 <luisgdr.25@gmail.com>
2022-01-18 23:12:55 -0500

    Funcionalidad búsqueda

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { forkJoin } from 'rxjs';
+import { TheMovieDBService } from '../services/api/themoviedb.service';
+import { ModalService } from '../services/modal.service';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +10,74 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  searchValue: string;
+  selectedValue: any;
+  page: number;
+  searchCardContainer: any = [];
+  loadingCurrentEventData: any;
 
+  constructor(
+    private service: TheMovieDBService,
+    private modalService: ModalService
+    ) {
+    this.searchValue = '';
+    this.selectedValue = 'movie';
+  }
+  filterList() {
+    this.page = 1;
+    this.searchCardContainer = [];
+
+    if (this.searchValue.length > 2) {
+      this.loadSearchContainer();
+    }
+
+  }
+  loadSearchContainer() {
+    this.service.getSearchList(this.selectedValue, this.page, this.searchValue).subscribe(res => {
+      //console.log(res);
+      //console.log(this.selectedValue);
+      res.results.forEach(el => {
+        this.searchCardContainer.push({
+          id: el.id,
+          title: this.selectedValue==='movie'?el.title:el.name,
+          description: el.overview,
+          image: (el.backdrop_path || el.poster_path)?('http://image.tmdb.org/t/p/original/' +
+          (el.backdrop_path || el.poster_path)):
+          'https://www.delivery.sv/a/img/no-disponible.png',
+          rating: el.vote_average,
+          modelItem: el
+        });
+      });
+      if (this.page > 1) {
+        this.loadingCurrentEventData.target.complete();
+        if (res.results.length === 0) {
+          this.loadingCurrentEventData.target.disabled = true;
+        }
+      }
+
+    });
+  }
+
+  loadData(event) {
+    this.page = this.page + 1;
+    this.loadingCurrentEventData = event;
+    this.loadSearchContainer();
+  }
+
+  selectionChanged() {
+    this.searchCardContainer = [];
+    this.searchValue = '';
+    this.page = 1;
+  }
+
+  cardEventListener(modelItem) {
+    forkJoin(this.service.getDetalleById(this.selectedValue, modelItem.id),
+      this.service.getCreditosById(this.selectedValue, modelItem.id),
+      this.service.getVideosById(this.selectedValue, modelItem.id)).subscribe(res=>{
+      modelItem.detailResponse = res[0];
+      modelItem.creditsResponse = res[1];
+      modelItem.videos = res[2];
+      this.modalService.presentModal(modelItem, this.selectedValue);
+    });
+  }
 }
